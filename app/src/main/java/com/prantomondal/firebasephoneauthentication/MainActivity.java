656d15f6d3e627fff17897a380229db5cfa67107
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-16 22:10:17 +0600

    Java File modified

--- app/src/main/java/com/prantomondal/firebasephoneauthentication/MainActivity.java
+++ app/src/main/java/com/prantomondal/firebasephoneauthentication/MainActivity.java
@@ -2,13 +2,28 @@ package com.prantomondal.firebasephoneauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.prantomondal.firebasephoneauthentication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    //biewbinding
+    private ActivityMainBinding binding;
+    //if code send failed ,will used to resend code
+    private PhoneAuthProvider .ForceResendingToken forceResendingToken;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    private String mVerificationId; //will hold OTP/Verification code
+
+    private static final String TAG = "MAIN_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
